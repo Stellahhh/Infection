3fe6ddfdff1dd6ede32db51d1667418c83b0891c
yfan43 <yfan43@jh.edu>
2025-02-23 18:02:32 -0500

    chase update

--- Dead_Line/Assets/ZombieFSM.cs
+++ Dead_Line/Assets/ZombieFSM.cs
@@ -18,27 +18,28 @@ public class EnemyFSM : MonoBehaviour
         ChasePlayer();
     }
 
-   void ChasePlayer()
-    {
+    void ChasePlayer() {
+
         if (sightSensor.detectedObject != null) 
-        {
-            agent.isStopped = false;
-            agent.SetDestination(sightSensor.detectedObject.transform.position); // Continuously update position
-            print("Chasing player");
-        }
-        else 
-        {
-            agent.isStopped = true; // Stop if the player is not detected
+            {
+                agent.isStopped = false;
+                agent.SetDestination(sightSensor.detectedObject.transform.position); 
+                print("Chasing Player: " + sightSensor.detectedObject.transform.position);
+                print("Agent Velocity: " + agent.speed);
+            }
+        else {
+            agent.isStopped = true; 
             print("Player not detected");
         }
     }
+
     
-    private void OnTriggerEnter(Collider other)
-    {
-        print("colliding with: " + other.gameObject.name);
-        if (other.gameObject.CompareTag("Player")) // Ensure the collider belongs to the player
-        {
-            agent.isStopped = true; // Stop movement
-        }
-    }
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     print("colliding with: " + other.gameObject.name);
+    //     if (other.gameObject.CompareTag("Player")) // Ensure the collider belongs to the player
+    //     {
+    //         agent.isStopped = true; // Stop movement
+    //     }
+    // }
 }
