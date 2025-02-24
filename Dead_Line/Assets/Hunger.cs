edc7f8dc4ad276bb5a96bd10eaea758b122a13d4
HanbeiZhou <hzhou43@jhu.edu>
2025-02-23 19:35:06 -0500

    added life in human and hunger in zombie

---
+++ Dead_Line/Assets/Hunger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunger : MonoBehaviour
+{
+    public float maxTime;
+    private float remainingTime;
+    
+    public UnityEngine.Events.UnityEvent onDeath;
+
+    private void Start()
+    {
+        remainingTime = maxTime;
+    }
+    void Update()
+    {
+        remainingTime -= Time.deltaTime; // Decrease time gradually
+
+        if (remainingTime < 0)
+        {
+            onDeath.Invoke();
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Human")) // Check if collided with a human
+        {
+            remainingTime = maxTime; // clear hungriness if eat a human
+        }
+    }
+}
